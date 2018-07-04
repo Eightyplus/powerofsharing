f3ff47dfa777588f3074e7aafb3929607e9d6edf
Morten Friesgaard <friesgaard@gmail.com>
2018-07-04 14:40:00 +0200

    adds photo of speakers

--- app/src/main/java/no/soperasteria/powerofsharing/RecyclerAdapter.kt
+++ app/src/main/java/no/soperasteria/powerofsharing/RecyclerAdapter.kt
@@ -1,14 +1,15 @@
 package no.soperasteria.powerofsharing
 
-import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-
 import org.jsoup.Jsoup
-
+import java.net.URL
 import kotlin.concurrent.thread
 
 /**
@@ -18,18 +19,20 @@ import kotlin.concurrent.thread
 class RecyclerAdapter(private val notify: () -> Unit) : RecyclerView.Adapter<RecyclerAdapter.AdapterHolder>() {
 
     private val speakers = mutableListOf<Speaker>()
+    private val photos = HashMap<Int, Bitmap>()
 
     init {
         getItems()
     }
 
-    private fun readSpeakers( notify: () -> Unit) {
+    private fun readSpeakers() {
 
         Jsoup.connect("https://powerofsharing.no/").get().run {
-            select(".speakers__person").forEachIndexed { index, element ->
+            select(".speakers__person").forEachIndexed { _, element ->
                 val name = element.select(".speakers__name").text()
                 val post = element.select(".speakers__post").text()
-                val photo = element.select(".speakers__photo").text()
+                val style = element.select(".speakers__photo").attr("style")
+                val photo = style.substring(style.indexOf("https://"), style.indexOf(".jpg") + 4)
                 val details = element.attr("href")
 
                 speakers.add(Speaker(name = name, post = post, photo = photo, details = details))
@@ -43,6 +46,7 @@ class RecyclerAdapter(private val notify: () -> Unit) : RecyclerView.Adapter<Rec
 
         internal var name: TextView = itemView.findViewById(R.id.name) as TextView
         internal var post: TextView = itemView.findViewById(R.id.post) as TextView
+        internal var photo: ImageView = itemView.findViewById(R.id.photo) as ImageView
 
         init {
             itemView.setOnClickListener(this)
@@ -55,7 +59,7 @@ class RecyclerAdapter(private val notify: () -> Unit) : RecyclerView.Adapter<Rec
 
     private fun getItems() {
         thread {
-            readSpeakers(notify)
+            readSpeakers()
         }
     }
 
@@ -71,6 +75,24 @@ class RecyclerAdapter(private val notify: () -> Unit) : RecyclerView.Adapter<Rec
 
         holder.name.text = speaker.name
         holder.post.text = speaker.post
+
+        val bitmap = getPhoto(position)
+        if (bitmap != null) {
+            holder.photo.setImageBitmap(bitmap)
+        }
+
+        thread {
+            val url = URL(speaker.photo)
+            val bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+            photos[position] = bmp
+            notify()
+        }
+    }
+
+    private fun getPhoto(position: Int): Bitmap? {
+        return if (position < photos.size) {
+            photos[position]
+        } else null
     }
 
     override fun getItemCount(): Int {

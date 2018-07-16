b382ff9a76b288d4dabf3f04ffd92088ab278c22
Morten Friesgaard <friesgaard@gmail.com>
2018-07-16 11:44:34 +0200

    flips counter

--- app/src/main/java/no/soperasteria/powerofsharing/fragment/CounterFragment.kt
+++ app/src/main/java/no/soperasteria/powerofsharing/fragment/CounterFragment.kt
@@ -1,5 +1,7 @@
 package no.soperasteria.powerofsharing.fragment
 
+import android.animation.Animator
+import android.animation.AnimatorSet
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -7,14 +9,9 @@ import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.widget.LinearLayout
-import android.widget.TextView
 import no.soperasteria.powerofsharing.R
 import no.soperasteria.powerofsharing.view.TextFlip
-import no.soperasteria.powerofsharing.view.animateIn
-import no.soperasteria.powerofsharing.view.animateOut
-
 
 class CounterFragment : DialogFragment() {
 
@@ -23,20 +20,15 @@ class CounterFragment : DialogFragment() {
     private var textViews = mutableListOf<TextFlip>()
 
     var text: String = ""
-      set(value) {
-          field = value
-          mHandler.post {
-              visualize(value)
-          }
-      }
-
-    private lateinit var animateIn: Animation
-    private lateinit var animateOut: Animation
+        set(value) {
+            field = value
+            mHandler.post {
+                visualize(value)
+            }
+        }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         linearLayout = inflater.inflate(R.layout.fragment_counter, container, false) as LinearLayout
-        animateIn = animateIn(layoutInflater.context)
-        animateOut = animateOut(layoutInflater.context)
         return linearLayout
     }
 
@@ -47,39 +39,34 @@ class CounterFragment : DialogFragment() {
     }
 
     private fun visualize(text: String) {
+        val list = mutableListOf<Animator>()
         val chars: CharArray = text.toCharArray()
         for (i in 0..(chars.size-1)) {
-            val char = chars[i]
-            val textFlip = textViewFor(i)
-
-            if (textFlip.currentChar != char) {
-                textFlip.currentChar = char
-                textFlip.visibleText.animation = animateIn
+            textViewFor(i).animate(chars[i])?.let {
+                list.add(it)
             }
         }
-        animateIn.start()
+
+        AnimatorSet().apply {
+            playTogether(list)
+        }.start()
     }
 
     private fun textViewFor(index: Int): TextFlip {
         return if (index < textViews.size) {
             textViews[index]
         } else {
-            create('!')
+            create(' ')
         }
     }
 
     private fun create(char: Char): TextFlip =
-            (layoutInflater.inflate(R.layout.textview_counter, linearLayout, false) as LinearLayout).run {
+            layoutInflater.inflate(R.layout.textview_counter, linearLayout, false).run {
                 linearLayout.addView(this)
-                TextFlip(findViewById(R.id.hidden), findViewById(R.id.visible)).apply {
-                    currentChar = char
+                TextFlip(findViewById(R.id.text1), findViewById(R.id.text2)).apply {
+                    init(char)
                     textViews.add(this)
                 }
             }
-
-    private fun animateText(textView: TextView, text: String) {
-        textView.animation = animateIn
-        textView.text = text
-    }
 
 }
--- app/src/main/java/no/soperasteria/powerofsharing/view/TextFlip.kt
+++ app/src/main/java/no/soperasteria/powerofsharing/view/TextFlip.kt
@@ -1,29 +1,36 @@
 package no.soperasteria.powerofsharing.view
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.content.Context
-import android.view.animation.Animation
-import android.view.animation.TranslateAnimation
 import android.widget.TextView
 import no.soperasteria.powerofsharing.R
 
 fun height (context: Context) = context.resources.getDimension(R.dimen.counterHeight)
 
-fun animateIn (context: Context) : Animation =
-        TranslateAnimation(0f, 0f, -height(context), 0f).apply {
-            duration = 750
-        }
+class TextFlip (val text1: TextView, val text2: TextView) {
 
-fun animateOut (context: Context) : Animation =
-        TranslateAnimation(0f, 0f, 0f, height(context)).apply {
-            duration = 750
-        }
+    private val animateIn = AnimatorInflater.loadAnimator(text1.context, R.animator.flip) as AnimatorSet
 
-class TextFlip (val hiddenText: TextView, val visibleText: TextView) {
+    fun init(char: Char) {
+        currentChar = char
+        text1.text = " "
+        text2.text = "$char"
+    }
 
-    var currentChar: Char = '$'
-        set(value) {
-            field = value
-            visibleText.text = "$value"
-            hiddenText.text = "$value"
-        }
+    private var currentChar: Char = '$'
+
+    fun animate(char: Char) : Animator? {
+        if (currentChar == char) return null
+        currentChar = char
+
+        text1.clearAnimation()
+        text2.text = text1.text
+
+        animateIn.setTarget(text1)
+        text1.text = "$currentChar"
+
+        return animateIn
+    }
 }
